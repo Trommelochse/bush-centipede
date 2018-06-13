d492c28f8558f21fa823fbf7230bebbfb1922766
Trommelochse <clemens.a.janes@gmail.com>
2018-06-14 01:26:03 +0200

    cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // App config
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); 
+app.use(cors);
 
 // DB config
 const mongoose = require('mongoose');
